1d8a9fc49bdea48c0ef9c40133b570c391532f17
Austin Powell <austinpowell58@gmail.com>
2018-07-07 02:08:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,31 +9,18 @@ function nestedTarget() {
 
 function deepestChild() {
   const start = document.getElementById('grand-node').querySelectorAll('div')
-  function find(start) {
-  // initialize two variables, `current`, and `next`
-  // `current` keeps track of the element that we're
-  // currently on, just like we did when unpacking the
-  // array above; `next` is itself an array that keeps
-  // track of the elements (which might be arrays!) that
-  // we haven't looked at yet
+  
   let current = start
   let next = []
  
-  // hey, a `while` loop! this loop will only
-  // trigger if `current` is truthy — so when
-  // we exhaust `next` and, below, attempt to
-  // `shift()` `undefined` (when `next` is empty)
-  // onto `current`, we'll exit the loop
   while (current) {
     // if `current` satisfies the `criteriaFn`, then
     // return it — recall that `return` will exit the
     // entire function!
-    if (criteriaFn(current)) {
+    if (current.inn {
       return current
     }
- 
-    // if `current` is an array, we want to push all of
-    // its elements (which might be arrays) onto `next`
+  }
     if (Array.isArray(current)) {
       for (let i = 0; i < current.length; i++) {
         next.push(current[i])
